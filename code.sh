96e9fd1ea8cb0c6084232160780e4d7a17ea2898
kanav gupta <60663545+guptakanav1999@users.noreply.github.com>
2020-08-10 13:36:20 +0530

    Update code.sh

--- code.sh
+++ code.sh
@@ -3,7 +3,7 @@ if kubectl get pods | grep httpd
 then
 echo "service is running"
 else
-kubectl create -f /mycode/http.yaml
+kubectl create -f /code/http.yaml
 sleep 20
 fi
 POD=$(kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")

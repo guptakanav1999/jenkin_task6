17184b85e069284e3b4a4217eb0f7252ffe6750e
kanav gupta <60663545+guptakanav1999@users.noreply.github.com>
2020-08-10 12:37:58 +0530

    Create cose.sh

---
+++ cose.sh
@@ -0,0 +1,10 @@
+
+if kubectl get pods | grep httpd
+then
+echo "service is running"
+else
+kubectl create -f /mycode/http.yaml
+sleep 20
+fi
+POD=$(kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")
+kubectl cp /code/*.html $POD:/var/www/html/
